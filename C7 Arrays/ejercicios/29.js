039e503727500c861a8248d1c0e703c640346343
Romina <rominagazzera@gmailcom>
2024-02-14 18:30:15 +0100

    Resuelto

--- C7 Arrays/ejercicios/29.js
+++ C7 Arrays/ejercicios/29.js
@@ -4,6 +4,17 @@ function encontrarNumeroFaltante(numeros) {
   // y retórnalo.
   // Devuelve null si el aray es vacío o si no hay números faltantes.
   // Tu código:
-}
+    if (numeros.length === 0) {
+      return null;
+    }
+    const n = numeros.length + 1;
+    const sumaTotal = (n * (n + 1)) / 2; 
+    const sumaActual = numeros.reduce((acc, num) => acc + num, 0);
+    const numeroFaltante = sumaTotal - sumaActual;
+    if (numeroFaltante === 0) {
+      return null;
+    }
+    return numeroFaltante;
+  }
 
 module.exports = encontrarNumeroFaltante;
--- C7 Arrays/ejercicios/30.js
+++ C7 Arrays/ejercicios/30.js
@@ -2,6 +2,14 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
+  const elementosVistos = {};
+  for (let i = 0; i < numeros.length; i++) {
+    const elemento = numeros[i];
+    if (elementosVistos[elemento]) {
+      return elemento;
+    } else {
+      elementosVistos[elemento] = true;
+    }
 }
-
+}
 module.exports = encontrarElementoRepetido;
